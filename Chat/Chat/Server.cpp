96552acbaac1a5e79a941169b97f458e3fc76351
Madalin <cazanmadalin12@gmail.com>
2025-02-19 23:39:49 +0200

    Multiple ferestre de client funcionale

--- Chat/Chat/Server.cpp
+++ Chat/Chat/Server.cpp
@@ -1,8 +1,7 @@
 ﻿#include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <vector>
-#include <string>
+#include <thread>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -10,7 +9,7 @@ void initializeWinsock() {
     WSADATA wsaData;
     int wsInit = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (wsInit != 0) {
-        std::cout << "eroare WSAStartup  " << wsInit << std::endl;
+        std::cout << "Eroare WSAStartup " << wsInit << std::endl;
         exit(1);
     }
 }
@@ -19,12 +18,34 @@ void cleanUpWinsock() {
     WSACleanup();
 }
 
+void clientHandler(SOCKET clientSocket, sockaddr_in clientAddr) {
+    char buffer[1024];
+    int recvResult;
+
+    // Se primește mesajul de la client
+    while ((recvResult = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+        buffer[recvResult] = '\0';
+
+        // Folosim inet_ntop pentru a obține IP-ul clientului
+        char ipAddress[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(clientAddr.sin_addr), ipAddress, INET_ADDRSTRLEN);
+
+        std::cout << "Client (" << ipAddress << "): " << buffer << std::endl;
+    }
+
+    if (recvResult == SOCKET_ERROR) {
+        std::cout << "Eroare la primirea datelor de la client!" << std::endl;
+    }
+
+    closesocket(clientSocket);
+}
+
 int main() {
     initializeWinsock();
 
     SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == INVALID_SOCKET) {
-        std::cout << "nu merge socket-ul" << std::endl;
+        std::cout << "Crearea socket-ului a eșuat!" << std::endl;
         cleanUpWinsock();
         return 1;
     }
@@ -32,54 +53,42 @@ int main() {
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(9000);  // Portul 9000
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;  // Ascultă pe orice adresă
 
     if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        std::cout << "Binding-ul esuat!" << std::endl;
+        std::cout << "Binding-ul a eșuat!" << std::endl;
         closesocket(serverSocket);
         cleanUpWinsock();
         return 1;
     }
 
     if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-        std::cout << "eroare la ascultarea pe socket!" << std::endl;
+        std::cout << "Eșec la ascultarea pe socket!" << std::endl;
         closesocket(serverSocket);
         cleanUpWinsock();
         return 1;
     }
 
-    std::cout << "Serverul asculta pe portul 9000..." << std::endl;
-
-    std::vector<SOCKET> clientSockets;
+    std::cout << "Serverul ascultă pe portul 9000..." << std::endl;
 
     while (true) {
         SOCKET clientSocket;
         sockaddr_in clientAddr;
         int clientAddrSize = sizeof(clientAddr);
-        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
 
+        // Acceptă o conexiune de la un client
+        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
         if (clientSocket == INVALID_SOCKET) {
-            std::cout << "Conexiune client naspa, nu merge!" << std::endl;
+            std::cout << "Conexiune client eșuată!" << std::endl;
             continue;
         }
 
-        std::cout << "Client conectat!" << std::endl;
-        clientSockets.push_back(clientSocket);
+        char ipAddress[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(clientAddr.sin_addr), ipAddress, INET_ADDRSTRLEN);
+        std::cout << "Client conectat: " << ipAddress << std::endl;
 
-        char buffer[1024];
-        int recvResult;
-
-        while ((recvResult = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
-            buffer[recvResult] = '\0';
-            std::cout << "Client: " << buffer << std::endl;
-
-        }
-
-        if (recvResult == SOCKET_ERROR) {
-            std::cout << "Eroare la primirea datelor!" << std::endl;
-        }
-
-        closesocket(clientSocket);
+        // Creează un thread pentru a gestiona clientul
+        std::thread(clientHandler, clientSocket, clientAddr).detach();  // Detach pentru a rula în fundal
     }
 
     closesocket(serverSocket);
--- Chat/ClientApp/Client.cpp
+++ Chat/ClientApp/Client.cpp
@@ -6,56 +6,55 @@
 #pragma comment(lib, "ws2_32.lib")
 
 void initializeWinsock() {
-	WSADATA wsaData;
-	int wsInit = WSAStartup(MAKEWORD(2, 2), &wsaData); // Inițializare Winsock
-	if (wsInit != 0) {
-		std::cout << "WSAStartup eroarea " << wsInit << std::endl;
-		exit(1);
-	}
+    WSADATA wsaData;
+    int wsInit = WSAStartup(MAKEWORD(2, 2), &wsaData); // Inițializare Winsock
+    if (wsInit != 0) {
+        std::cout << "Eroare WSAStartup " << wsInit << std::endl;
+        exit(1);
+    }
 }
 
 void cleanUpWinsock() {
-	WSACleanup();
+    WSACleanup();
 }
 
 int main() {
-	initializeWinsock();
+    initializeWinsock();
 
-	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (clientSocket == INVALID_SOCKET) {
-		std::cout << "Crearea socket-ului a esuat!" << std::endl;
-		cleanUpWinsock();
-		return 1;
-	}
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cout << "Crearea socket-ului a esuat!" << std::endl;
+        cleanUpWinsock();
+        return 1;
+    }
 
-	sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(9000);  // Conectare la server pe portul 9000
-	inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);  // Adresa localhost
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(9000);  // Portul serverului
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);  // Adresa localhost
 
-	if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-		std::cout << "Conectarea la server a eșuat!" << std::endl;
-		closesocket(clientSocket);
-		cleanUpWinsock();
-		return 1;
-	}
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cout << "Conectarea la server a esuat!" << std::endl;
+        closesocket(clientSocket);
+        cleanUpWinsock();
+        return 1;
+    }
 
-	std::cout << "Conectat la server!" << std::endl;
+    std::cout << "Conectat la server!" << std::endl;
 
-	// Citim și trimitem cuvinte către server
-	std::string word;
-	std::cout << "Introdu un cuvant (sau 'exit' pentru a iesi): ";
-	while (true) {
-		std::getline(std::cin, word);
-		std::cout << "->";
-		if (word == "exit") {
-			break;
-		}
+    // Citim și trimitem cuvinte către server
+    std::string word;
+    std::cout << "Introdu un cuvant (sau 'exit' pentru a iesi): ";
+    while (true) {
+        std::getline(std::cin, word);
+        if (word == "exit") {
+            break;
+        }
 
-		send(clientSocket, word.c_str(), word.size(), 0);  // Trimitere cuvânt la server
-	}
+        send(clientSocket, word.c_str(), word.size(), 0);  // Trimitere cuvânt la server
+    }
 
-	closesocket(clientSocket);
-	cleanUpWinsock();
-	return 0;
+    closesocket(clientSocket);
+    cleanUpWinsock();
+    return 0;
 }
